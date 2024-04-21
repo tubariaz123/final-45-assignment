21b656790d563ad04dfe710921ddcd5ee007c0cd
Tuba Riaz <161192504+tubariaz123@users.noreply.github.com>
2024-04-21 15:47:11 +0500

    Add files via upload
    add an optional extended description

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assignment # 1
+// storing the person name in a variable
+let personName = "Tuba";
+// Printing a message to the person
+console.log(`hello ${personName} would you like to learn some Python today?`);
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Assignment # 1
+
+// storing the person name in a variable
+
+let personName:string = "Tuba"
+
+// Printing a message to the person
+
+
+console.log(`hello ${personName} would you like to learn some Python today?`)
